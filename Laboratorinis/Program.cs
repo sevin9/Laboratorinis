71e671a9fe26eab0913bed0db51c7963e5431db6
sevin9 <Sevin9@gmail.com>
2020-04-17 17:47:05 +0300

    Meniu

--- Laboratorinis/Program.cs
+++ Laboratorinis/Program.cs
@@ -6,7 +6,25 @@ namespace Laboratorinis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Iveskite norima komanda:");
+            Console.WriteLine("Iveskite norima komanda:");
+
+            bool Isejimas = true;
+            while(Isejimas)
+            {
+                switch (Console.ReadLine())
+                {
+                    default:
+                        Console.WriteLine("Nerasta komanda");
+                        Main(args);
+                        break;
+                    case "exit":
+                        Isejimas = false;
+                        break;
+                }
+            }
+                
         }
     }
 }
